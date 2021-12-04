e8e77fa3247fd85a3bc2a5128ce7250605d6fbc1
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2021-12-04 14:16:27 +0100

    Day 4 solutions

--- 2021/d4p2.py
+++ 2021/d4p2.py
@@ -19,19 +19,21 @@ class board():
             r += '\n'
         return r
 
+    def reset_scores(self):
+        self.scores = ([0] * self.N, [0] * self.N) # (row scores, column scores)
+
     def is_winner(self):
-        if self.N in self.scores[0] + self.scores[1]:
-            return 1
+        if (self.N in self.scores[0]) or (self.N in self.scores[1]):
+            return True
         else:
-            return 0
+            return False
 
     def draw_number(self, number):
         c = self.find_number(number)
         if c is None:
-            return 0
+            return False
         self.scores[0][c[0]] += 1
         self.scores[1][c[1]] += 1
-        print(self.is_winner())
         return self.is_winner()
 
     def find_number(self, number):
@@ -55,17 +57,31 @@ class bingo():
         self.draws = draws
         self.D = len(draws)
 
-    def play_game(self):
+    def reset_scores(self):
+        for b in self.boards:
+            b.reset_scores()
+
+    def play_game(self, mode):
+        self.reset_scores()
+        winner_boards = []
+        winner_draws = []
         for i in range(self.D):
             for b in self.boards:
                 w = b.draw_number(self.draws[i])
-                print(b.scores)
-                if w: 
-                    return b, self.draws[:i+1]
+                if w:
+                    # print(i, self.draws[i])
+                    if mode == 'first': 
+                        return b, self.draws[:i+1]
+                    elif b not in winner_boards:
+                        winner_boards.append(b)
+                        winner_draws.append(self.draws[i])
+        if mode == 'last':
+            # print(winner_boards[-1], self.draws[:self.draws.index(winner_draws[-1]) + 1]
+            return winner_boards[-1], self.draws[:self.draws.index(winner_draws[-1]) + 1]
             # time.sleep(1)
 
-    def get_result(self):
-        winner, drawn = self.play_game()
+    def get_result(self, mode = 'first'):
+        winner, drawn = self.play_game(mode)
         return winner.get_unmarked_sum(drawn) * drawn[-1]
 
 
@@ -84,6 +100,5 @@ for i in range(L):
 #     print(b)
 
 game = bingo(boards, draws)
-result = game.get_result()
-
-print(result)
+print(f"Day 4 Part 1 solution: {game.get_result('first')}")
+print(f"Day 4 Part 2 solution: {game.get_result('last')}")
