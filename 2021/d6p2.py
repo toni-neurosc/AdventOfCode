8492dc8ee9bb9cd012ef8487ea1706913d897c86
Toni M. Brotons <10654467+toni-neurosc@users.noreply.github.com>
2021-12-06 12:46:47 +0100

    Add comments

--- 2021/d6p2.py
+++ 2021/d6p2.py
@@ -8,19 +8,23 @@ parser.add_argument('file', type=argparse.FileType('r')) # Input file
 args = parser.parse_args()
 
 def grow_lanterfish(fish_count, days):
+    # Takes a list of length equal to rate, where each position contains an integer representing
+    # the number of fish that have a number of days until replication equal to the position index
+    # Then simulated a number of replication cycles equal to the "days" parameter in a recursive fashion
+
     # print(days)
     # print(fish_count)
     if days > 0:
-        new_fish = fish_count[0]
-        for i in range(rate+1):
-            fish_count[i] = fish_count[i+1]
-        fish_count[rate-1] += new_fish
-        fish_count[-1] = new_fish
+        new_fish = fish_count[0] # Store the number of fish due for replication
+        for i in range(rate+1): 
+            fish_count[i] = fish_count[i+1] # Move all the fish that have more than 0 days until reproduction one position down
+        fish_count[rate-1] += new_fish # Reset the replication stage of the fish that have just replicated and add them to the proper stage
+        fish_count[-1] = new_fish # Add the corresponding newly spawned fish
         return grow_lanterfish(fish_count, days-1)
     else:
         return fish_count
 
-fish = [int(n) for n in args.file.readline().strip().split(',')]
+fish = [int(n) for n in args.file.readline().strip().split(',')] # Count fish for each stage of the reproduction cycle
 days = args.days
 rate = args.rate
 
